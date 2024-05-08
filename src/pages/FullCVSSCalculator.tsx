1f7d31568783ddfde0733a17dbe040f17ec838ab
bfuentesc <brauliofuentescontreras@gmail.com>
2024-05-07 23:46:36 -0400

    fix reponsive issue

--- src/pages/FullCVSSCalculator.tsx
+++ src/pages/FullCVSSCalculator.tsx
@@ -52,7 +52,9 @@ const FullCVSSCalculator = () => {
 
     const [vectorValues, setVectorValues] = useState<VectorValues>(initialVectorValues);
 
-    const [baseScore, setBaseScore] = useState<null | number>(null)
+    const [baseScore, setBaseScore] = useState<null | number>(null);
+    const [impact, setImpact] = useState<null | number>(null);
+    const [explotability, setExplotability] = useState<null | number>(null);
 
     const [vector, setVector] = useState<null | string>(null)
 
@@ -71,8 +73,8 @@ const FullCVSSCalculator = () => {
 
     useEffect(() => {
         if (cvssVector) {
-            setVector(`CVSS:3.1/${cvssVector}`);
-            const values = parseCVSSVector(`CVSS:3.1/${cvssVector}`);
+            setVector(`${cvssVector}`);
+            const values = parseCVSSVector(`${cvssVector}`);
             setVectorValues(values);
         }
 
@@ -90,7 +92,10 @@ const FullCVSSCalculator = () => {
 
             setVector(generateCVSSVector(vectorValues));
             // setValues(numericCVSSValue);
-            setBaseScore(calculateCVSS(numericCVSSValue));
+            const { BaseScore, Impact, Exploitability } = calculateCVSS(numericCVSSValue);
+            setBaseScore(BaseScore);
+            setImpact(Impact);
+            setExplotability(Exploitability);
 
         }
     }, [vectorValues]); // Depende del estado 'values' para re-ejecutarse cuando cambie
@@ -107,14 +112,29 @@ const FullCVSSCalculator = () => {
 
         }}>
         <CVSSDisplay baseScore={baseScore} vectorComponent={
-            <Typography variant="h6" sx={{ m: 1, fontSize: "100%" }}>
+            <Typography
+                variant="h6"
+                sx={{
+                    m: 1,
+                    fontSize: "100%",
+                    whiteSpace: 'normal', // Asegura que el espacio en blanco dentro del texto se maneje normalmente
+                    overflowWrap: 'break-word', // Permite que las palabras largas sean partidas y pasen a la siguiente línea
+                    wordBreak: 'break-word' // Asegura que la ruptura de palabras se haga correctamente en todos los navegadores
+                }}
+            >
                 <Link
                     to={`/full?cvss_vector=${vector}`}
-                    style={{ textDecoration: 'none', color: theme.palette.primary.main }}
+                    style={{
+                        textDecoration: 'none',
+                        color: theme.palette.primary.main,
+                        display: 'block', // Hace que el enlace se comporte como un bloque permitiendo el ajuste de línea
+                        overflowWrap: 'break-word', // Aplica también el ajuste en el nivel del enlace si es necesario
+                    }}
                 >
                     {vector}
                 </Link>
             </Typography>
+
         } />
         <Box sx={{
             display: 'flex',
